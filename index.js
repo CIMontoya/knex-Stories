a9a6444a7212852a63514f8cfe0a06b7a6e6784f
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-02-28 12:08:05 -0700

    added dotenv

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const knex = require('knex')(config)
 
+const dotenv = require('dotenv').config()
+
+
+
+
 app.get('/', (req, res, nex) => {
   knex('users')
   .then(users => {
